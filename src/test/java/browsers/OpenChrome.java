6b3b3f61e39fe074cd79a1bbe7ac16e8c088641f
Thuy Huynh <thuyhuynh@Thuys-MacBook-Pro.local>
2020-10-17 15:55:53 +0700

    init projcect

---
+++ src/test/java/browsers/OpenChrome.java
@@ -0,0 +1,19 @@
+package browsers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class OpenChrome {
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        //WebDriverManager .chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        //driver.get("https://google.com.vn");
+        driver.navigate().to("https://google.com.vn");
+        driver.manage().window().maximize();
+
+        driver.close();
+        //driver.quit();
+    }
+}
---
+++ src/test/java/browsers/OpenSafari.java
@@ -0,0 +1,9 @@
+package browsers;
+
+import org.openqa.selenium.safari.SafariDriver;
+
+public class OpenSafari {
+    public static void main(String[] args) {
+        new SafariDriver();
+    }
+}
