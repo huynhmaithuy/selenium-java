cc6dd042f7f6133b1bfd4cfa53fcd54aa3936da0
huynhmaithuy <67060955+huynhmaithuy@users.noreply.github.com>
2020-10-31 15:56:25 +0700

    Day3 (#2)
    * add login test with valid credentials
    
    * add checkbox, dropdown, hyperlink basicauth
    
    * day3
    
    Co-authored-by: Thuy Huynh <thuyhuynh@Thuys-MacBook-Pro.local>

---
+++ src/test/java/findElement/DynamicLoadingPage.java
@@ -0,0 +1,32 @@
+package findElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DynamicLoadingPage {
+    @Test
+    void loadingTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.xpath("//div[@id='start']/button")).click();
+        //driver.findElement(By.xpath("//button[.='Start']")).click();
+        //Thread.sleep(10000);
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(driver.findElement(By.x)))
+        String finished = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")))
+                .findElement(By.tagName("h4")).getText().trim();
+
+
+
+        //String finished = driver.findElement(By.xpath("//*[@id='finish']/h4")).getText().trim();
+        Assert.assertEquals(finished,"Hello World!");
+    }
+}
---
+++ src/test/java/findElement/JSAlerts.java
@@ -0,0 +1,40 @@
+package findElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class JSAlerts {
+
+    @Test
+    void validatePopUpDisplay(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[contains(.,'Click for JS Alert')]")).click();
+        driver.switchTo().alert().accept();
+        String result = driver.findElement(By.id("result")).getText().trim();
+        Assert.assertEquals(result, "You successfuly clicked an alert");
+
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+        driver.switchTo().alert().dismiss();
+        result = driver.findElement(By.id("result")).getText().trim();
+        Assert.assertEquals(result, "You clicked: Cancel");
+
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+        driver.switchTo().alert().accept();
+        result = driver.findElement(By.id("result")).getText().trim();
+        Assert.assertEquals(result, "You clicked: Ok");
+
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+        driver.switchTo().alert().sendKeys("hello");
+        driver.switchTo().alert().accept();
+        result = driver.findElement(By.id("result")).getText().trim();
+        Assert.assertEquals(result, "You entered: hello");
+
+    }
+}
